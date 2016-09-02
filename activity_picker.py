6e20b054eee5d747e1c4bdf1347ca8a6f0905be7
willsauer <bensauer+will@gmail.com>
2016-09-01 17:02:55 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,12 +1,10 @@
 import random, urllib2
 # mport special libraries already built in python
 import random
-the_url = "https://raw.githubusercontent.com/bellcodo/congenial-octo-meme/66b0e3c28db56b0abd536f0d9756d0e0f182ea93/activities.lst"
+the_url = "https://raw.githubusercontent.com/willsauer/congenial-octo-meme/master/activities.lst"
 list_raw_text = urllib2.urlopen(the_url).read()
 # list of options to select from
-
-possible_activities = ['a', 'b', 'c']
-the_url = "raw.githubusercontent.com/bellcodo/congenial-octo-meme"
+import random
 # choice of what we are going to do
 print "debug " + str(list_raw_text.split())
 possible_activities = list_raw_text.split()
@@ -14,3 +12,5 @@ possible_activities = list_raw_text.split()
 the_activity = random.choice(possible_activities)
 print "Possible activities are: " + str(possible_activities)
 print "What we are going to do: " + the_activity
+banana = random.choice(possible_activities)
+print "what were not going to do: " + str(banana)
